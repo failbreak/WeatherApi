d4a4f026ff6c93120c681adbae195629cac1c236
FailBreak <maxx0696@elevcampus.dk>
2023-05-15 09:35:47 +0200

    Sad life

--- WorkerService/Worker.cs
+++ WorkerService/Worker.cs
@@ -22,18 +22,17 @@ namespace WorkerService
 
         public async Task GetData()
         {
+            //  Using Httpclient to get api data 
             HttpClient apiClient = new HttpClient();
             DataSet json = await apiClient.GetFromJsonAsync<DataSet>("https://dmigw.govcloud.dk/v2/metObs/collections/observation/items?period=latest-10-minutes&api-key=1fa153cf-5edb-40f7-996b-14adcdc497f4");
 
 
-
-            //List<Properties> props = json.Features.Select(x => ;
-            //List<Geometry> geo = json.Features.Select(x => new Geometry { Coordinates = x.Geometry.Coordinates.Select(y => new Cords { Value = y }).ToList(), Type = x.Geometry.Type }).ToList();
+            // mapping everything
             List<Features> feat = json.Features.Select(x => new Features { Properties = new Properties { Created = x.Properties.Created, Observed = x.Properties.Observed, ParameterId = x.Properties.ParameterId, StationId = x.Properties.StationId, Value = x.Properties.Value }, Geometry = new Geometry { Coordinates = x.Geometry.Coordinates.Select(y => new Cords { Value = y }).ToList(), Type = x.Geometry.Type }, Type = x.Type, Id = x.Id }).ToList();
             Application app = new Application { Features = feat, Type = json.Type };
            
             
-
+            // saving
             using (WeatherContext context = serviceProvider.CreateAsyncScope().ServiceProvider.GetService<WeatherContext>())
             {
                 await context.AddAsync<Application>(app);
@@ -44,12 +43,12 @@ namespace WorkerService
 
         protected override async Task ExecuteAsync(CancellationToken stoppingToken)
         {
+            // stopwatch because we need this to run only every 10 minutes
             bool IsStart = true;
             Stopwatch stopwatch = new();
             while (!stoppingToken.IsCancellationRequested)
             {
-                //_logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
-                //await Task.Delay(1000,
+                
                 if (stopwatch.Elapsed.TotalMinutes >= 10 || IsStart)
                 {
                     IsStart = false;
